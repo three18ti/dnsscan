2520865aaee1b366b00281c16e61361e54d9eef2
three18ti <three18ti@gmail.com>
2013-04-08 20:23:39 -0600

    first "proof of concept" script works... mostly anyway.  seems to have trouble with some of my OO syntax...

---
+++ bin/dns-scanner.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use 5.010;
+
+use Net::IP;
+use Getopt::Long::Descriptive;
+
+use lib 'lib';
+use DNSScanner;
+
+my $range = $ARGV[0];
+
+my $scanner = DNSScanner->new({ verbose => 1 });
+
+my $ip = new Net::IP($range);
+$scanner->logger("Launching queries against " . $ip->print . ", in sets of " . $scanner->at_once . " at a time...") 
+    if $scanner->verbose;
+
+$ip = $scanner->launch_queries($ip);
+
+$scanner->logger($scanner->select->count . "queries running...") if $scanner->verbose;
+
+my @ready;
+
+while (keys $scanner->state) {
+    @ready = $scanner->select->can_read(2);
+    
+    if (@ready) {
+        foreach my $socket (@ready) {
+            $scanner->handle_result($socket);
+#            $scanner->queried = $scanner->queried + 1;
+
+            $scanner->select->remove($socket);
+
+#            $scanner->logger(scalar $scanner->state->keys . " remaining") if $scanner->verbose;
+        }
+    }
+    else {
+        my $now = time;
+
+        foreach my $socket ($scanner->select->handles) {
+            if ($now - $scanner->state->{$socket}->{when} > $scanner->timeout * 2) {
+                my $resolver_ip = $scanner->state->{$socket}->{ip};
+
+                $scanner->logger("Query for $resolver_ip timed out") if $scanner->verbose;
+
+#                $scanner->queried++;
+
+                $scanner->state->{$socket} = undef;
+
+                $scanner->select->remove($socket);
+
+                $scanner->state->{$socket}->delete;
+                
+            }
+        }
+    }
+    $ip = $scanner->launch_queries($ip);
+}
--- lib/DNSScanner.pm
+++ lib/DNSScanner.pm
@@ -1,5 +1,153 @@
+package DNSScanner;
+use 5.010;
 use strict;
 use warnings;
-package DNSScanner;
+
+use Moose;
+
+use Net::IP;
+use Net::DNS;
+use IO::Select;
+
+has 'select' => (
+    is      => 'ro',
+    isa     => 'IO::Select',
+    default => sub { IO::Select->new },
+);
+
+has 'state' => (
+    is      => 'rw',
+    isa     => 'HashRef',
+    default => sub { {} },
+);
+
+has 'queried' => (
+    is      => 'rw',
+    isa     => 'Int',
+    default => '0',
+);
+
+has 'start' => (
+    is      => 'ro',
+    isa     => 'Int',
+    default => sub { time },
+);
+
+# how many ips to scan at once
+has 'at_once' => (
+    is      => 'ro',
+    isa     => 'Int',
+    default => '100',
+);
+
+# domain to use for testing recursion
+has 'search_domain' => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => 'google.com',
+);
+
+# time inbetween requests
+has 'timeout'   => (
+    is      => 'ro',
+    isa     => 'Int',
+    default => '1',
+);
+
+# number of times to retry request
+has 'retry'     => (
+    is      => 'ro',
+    isa     => 'Int',
+    default => '2',
+);
+
+# Set verbose logging to STDERR
+has 'verbose' => (
+    is      => 'ro',
+    isa     => 'Int',
+    default => '0',
+);
+
+
+sub launch_queries {
+    my $self = shift;
+    my $ip = shift;
+
+    while ($ip and ($self->select->count < $self->at_once)) {
+        $self->check_resolver($ip);
+        $ip++
+    }
+    return $ip;
+}
+
+sub check_resolver {
+    my $self = shift;
+    my $resolver_ip = shift;
+
+    $self->logger("Checking " . $resolver_ip->ip ) if $self->verbose;
+
+    # create new DNS object
+    my $dns = Net::DNS::Resolver->new;
+
+    # set nameserver
+    $dns->nameservers($resolver_ip->ip);
+
+    # Ignore search list
+    # don't append anything to the end of the result
+    $dns->dnsrch(0);
+    $dns->defnames(0);
+
+    # allow timeout, default 1sec
+    $dns->retrans($self->timeout);
+
+    # allow retries, default 2
+    $dns->retry($self->retry);
+
+    my $socket = $dns->bgsend($self->search_domain, 'A', );
+
+    $self->state->{$socket}->{ip}   = $resolver_ip->ip;
+    $self->state->{$socket}->{dns}  = $dns;
+    $self->state->{$socket}->{when} = time;
+
+    $self->select->add($socket);
+}
+
+sub handle_result {
+    my $self = shift;
+    my $socket = shift;
+
+    (warn "Socket $socket does not exist" && next) unless exists $self->state->{$socket};
+
+    my $ip = $self->state->{$socket}->{ip};
+    my $dns = $self->state->{$socket}->{dns};
+
+    my $packet = $dns->bgread($socket);
+
+    unless (defined $packet and $packet->answer) {
+        $self->logger("No answer from $ip") if $self->verbose;
+    }
+    else {
+        say "[*] Got answer from $ip - Possible open resolver";
+        $packet->print if $self->verbose;
+    }
+    
+    $self->state->{$socket} = undef;
+#    delete $self->state-{$socket};
+}
+
+
+sub logger {
+    my $self = shift;
+    my $message = shift;
+    say STDERR "# " . scalar gmtime() . "| $message";
+}
+
+sub sanity_check {
+    my $self = shift;
+    my $dns = Net::DNS::Resolver->new;
+    my $query = $dns->search($self->search_domain, 'A');
+
+    return $query ? '1' : '0';
+}
 
 1;
